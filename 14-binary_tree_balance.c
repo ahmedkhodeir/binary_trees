f882f0eb101184037f9186a51acb60ed7431dff5
Ahmed Khodeir <110243896+ahmedkhodeir@users.noreply.github.com>
2023-10-30 09:57:36 +0200

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * binary_tree_height_b - Measures height of a binary
  * @tree: tree to go through
  * Return: the height
  */
