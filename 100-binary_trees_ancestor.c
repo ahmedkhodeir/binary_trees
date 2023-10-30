f1d63edc918e2bf1507adbc22a7766bcdcca0331
Ahmed Khodeir <110243896+ahmedkhodeir@users.noreply.github.com>
2023-10-30 09:52:54 +0200

    update

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the depth of a node in a binary tree.
- *@tree: pointer to the node to measure the depth.
- * Return: if tree is NULL, function must return 0.
+ * binary_tree_depth - measures the depth
+ *@tree: pointer to the node
+ * Return: if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
