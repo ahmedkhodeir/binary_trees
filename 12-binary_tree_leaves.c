014b3c0258f669f1ad1b983d153f291600171835
Ahmed Khodeir <110243896+ahmedkhodeir@users.noreply.github.com>
2023-10-30 09:59:41 +0200

    update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -  a function that checks if a node is a leaf.
- *@node: is a pointer to the node to check.
- * Return: return 1 if node is a leaf, otherwise 0.
+ * binary_tree_is_leaf -  a function that checks if a node.
+ *@node: is a pointer to the node.
+ * Return: return 1 if node is a leaf.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
