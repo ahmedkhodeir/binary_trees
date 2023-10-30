c954744d75780d6323268a41186d319b5949631d
Ahmed Khodeir <110243896+ahmedkhodeir@users.noreply.github.com>
2023-10-30 09:54:07 +0200

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,11 +2,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: tree to measure the height of
+ * binary_tree_height - measures the height.
+ * @tree: tree to measure.
  *
  * Return: height of the tree
- *         0 if tree is NULL
+ *
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
